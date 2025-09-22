278d1523c7a5ab56302252b5f75c39ecad172c74
Gustavo Victor Ferreira <ferreirag930@yahoo.com>
2025-09-22 15:02:05 -0300

    pra evitar cair energia

--- src/controllers/cosplayControllers.js
+++ src/controllers/cosplayControllers.js
@@ -12,6 +12,35 @@ const getAllCosplays = (req, res) => {
     });
 
 
+    const personagemCosplay = ["Monkey D. Luffy", "Levi Ackerman", "Sailor Moon", "Goku Super Saiyajin", "Sakura Kinomoto", "Naruto Uzumaki", "Kenshin Himura", "Eren Yeager", "Gojo Satoru", "Inosuke Hashibira"];
+
+    const animeCosplay = ["One Piece", "Attack on Titan", "Sailor Moon", "Dragon Ball Z", "Cardcaptor Sakura", "Naruto Shippuden", "Samurai X", "Attack on Titan", "Jujutsu Kaisen", "Demon Slayer"];
+
+    const eventoCosplay = ["Anime Friends 2024", "Comic Con Experience 2023", "Geek City 2022", "Ressaca Friends 2024", "Anime Summit 2023", "BGS 2023", "Anime Friends 2023", "Geek City 2023", "Anime XP 2024", "Anime Friends 2024"];
+
+    const custoCosplay = ["R$ 550.00", "R$ 800.00", "R$ 420.00", "R$ 650.00", "R$ 380.00", "R$ 500.00", "R$ 720.00", "R$ 780.00", "R$ 900.00", "R$ 680.00"];
+
+
+
+
+    //Filtros
+      if (personagem) {
+        resultado = personagem.filter((p) => p.personagem.toLowerCase() === personagem.toLowerCase())
+      }
+    
+      if (anime) {
+        resultado = anime.filter((a) => a.anime.toLowerCase() === anime.toLowerCase())
+      }
+
+      if(evento) {
+        resultado = evento.filter((e) => e.evento.toLowerCase() === evento.toLowerCase())
+      }
+
+      if(custo) {
+        resultado = resultado.filter((c) => c.custo == custo);
+      }
+ };
+
     const getCosplayById= (req, res) => {
         const id = parseInt(req.params.id);
 
@@ -68,7 +97,7 @@ const getAllCosplays = (req, res) => {
             });
          }
     
-          if (!custo) {
+          if (custo = 0 && custo < 0) {
             return res.status(400).json({
                 success: false,
                 message: "O campo 'custo' é obrigatório"
@@ -89,12 +118,17 @@ const getAllCosplays = (req, res) => {
         
         //Regra de negócios
 
-        if (custo = 0 || custo < 0) {
+
+        //Custo deve ser maior que 0 reais
+        if (custo < 0 || custo === 0 ) {
             return res.status(404).json({
                 success:false,
                 message: "O custo não pode ser 0 reais e nem negativo",
             });
         }
+
+        //Data deve ser uma data válida e não pode ser futura
+
 
 
         const novoCosplay = {
